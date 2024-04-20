a1640ae3564baea17a8d57dd29a969bb35eb746c
Jose Germán Martínez <jgermanmm@gmail.com>
2024-04-20 19:37:17 -0400

    Agregar validación de URL en getServerURL en task1.js

--- task1.js
+++ task1.js
@@ -4,6 +4,7 @@
 // Definir la URL del servidor
 const serverURL = "http://localhost:3000";
 
+
 // Crear la función getServerURL
 export function getServerURL() {
  // chequear si la URL esta funcionando
