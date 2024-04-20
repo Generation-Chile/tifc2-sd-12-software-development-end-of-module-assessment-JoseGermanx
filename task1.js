24f9cf2e1e2d925cd6b32a49f14ab45b94e9e877
Jose Germán Martínez <jgermanmm@gmail.com>
2024-04-20 13:54:39 -0400

    Agregar funciones para obtener la URL del servidor, listar usuarios, agregar usuarios y eliminar usuarios

--- task1.js
+++ task1.js
@@ -1,9 +1,12 @@
 // Task 1: getServerURL()
 
 
+
 // Definir la URL del servidor
+const serverURL = "http://localhost:3000";
 
-
-// Exportar la función
-
-
+// Crear la función getServerURL
+export function getServerURL() {
+ // chequear si la URL esta funcionando
+    return serverURL;
+}
--- task3.js
+++ task3.js
@@ -1,2 +1,17 @@
 // Task 3: addUser(first_name, last_name, email)
 
+import * as getUrl from "./task1.js";
+export function addUser(first_name, last_name, email) {
+    const user = {
+        first_name: first_name,
+        last_name: last_name,
+        email: email
+    }
+    fetch(getUrl.getServerURL() + "/users", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+    }
